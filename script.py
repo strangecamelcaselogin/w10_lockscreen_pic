f3d3230ef4930fc2aee27bce5283b273ebb96ee7
strangecamelcaselogin <strangecamelcaselogin@gmail.com>
2017-08-16 17:27:49 +0300

    fix in username, make dest dir if not exist

--- script.py
+++ script.py
@@ -1,24 +1,24 @@
 import os
+from os import path
 import shutil
 
-USER = 'User'
-SOURCE = r'C:\Users\{}\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets'.format(USER)
-DEST = r'C:\Users\User\Desktop\Заставки'
 
-try:
-    file_list = os.listdir(path=SOURCE)
-    
-    real_index = 0
-    for index, filename in enumerate(file_list):
-        src = os.path.join(SOURCE, filename)
-        if not os.path.isdir(src) and \
-                (os.stat(src).st_size / 1024) > 100:
-                    
-            real_index += 1
-            dst = os.path.join(DEST, 'pic_'+str(real_index) + '.jpg')    
-            shutil.copy(src, dst)
-            
-            print('[ ok ] {}'.format(dst))
+USER = ''  # Windows username
+SOURCE = r'C:\Users\{}\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets'.format(USER)  # Let stay as is
+DEST = r'C:\Users\{}\Desktop\Заставки'.format(USER)  #  Destination path
+MIN_FILE_SIZE = 100 * 1024  # 100kb by default
 
-except Exception as e:
-    print(e)
+
+if not path.isdir(DEST):
+    os.mkdir(DEST)
+
+real_index = 0
+for filename in os.listdir(path=SOURCE):
+    src = path.join(SOURCE, filename)
+    if not path.isdir(src) and os.stat(src).st_size > MIN_FILE_SIZE:
+        dst = path.join(DEST, 'pic_' + str(real_index) + '.jpg')
+        
+        shutil.copy(src, dst)
+
+        real_index += 1
+        print('[ ok ] {}'.format(dst))
